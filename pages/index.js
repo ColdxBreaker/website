21583205fa36eb6d875346fca64a28d6e5cde872
Chris Lim <clim.dev@hotmail.com>
2020-01-04 23:24:53 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ const Index = () => (
     <div>
         <p><center>Coming Soon!</center></p>
         <p><center>Chris Lim © 2019</center></p>
+        <p><centerUpdates are rolling out throughout the year!</center></p>
     </div>
 )
 export default Index;
