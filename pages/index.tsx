bf8341c2a2d69b485ebaaed108c5da52edb4db40
Christopher Lim Tung Tseung <christopher.limtungtseung@mail.utoronto.ca>
2020-01-09 13:35:05 -0500

    Add index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Layout from '../hoc/Layout/Layout'
+import Home from '../components/Home/Home';
+import Header from "../hoc/Layout/Header/Header"
+import React from 'react';
+import "../css/Index.module.css"
+
+
+const Index = () => (
+    <Layout>
+        <Home/>
+    </Layout>
+);
+
+
+export default Index;
