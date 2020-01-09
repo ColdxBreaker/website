b84e7185cdd12cedf0994e84348e078ccfc77a09
Chris Lim <clim.dev@hotmail.com>
2020-01-08 22:00:26 -0500

    Delete index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-const Index = () => (
-    <div>
-        <p><center>Coming Soon!</center></p>
-        <p><center>Chris Lim © 2019</center></p>
-        <p><center>Updates are rolling out throughout the year!</center></p>
-    </div>
-)
-export default Index;
