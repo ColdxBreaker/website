85dedc7016f2a304850827ad40b54ae8b2b8c1fb
Christopher Lim Tung Tseung <christopher.limtungtseung@mail.utoronto.ca>
2020-03-14 12:25:42 -0400

    Add stylesheet

--- pages/_document.js
+++ pages/_document.js
@@ -11,8 +11,12 @@ class CustomDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"/>
-                    {/*https://fonts.googleapis.com/css?family=Roboto&display=swap*/}
+                    <link
+                        rel="stylesheet"
+                        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+                        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+                        crossOrigin="anonymous"
+                    />
                 </Head>
                 <body>
                     <Main/>
